0938b20a2c692bdee4fb31eec05764c7d5341c98
luzelin <luzelin1024@163.com>
2023-01-08 13:58:18 +0000

    tasks

--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -8,18 +8,47 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <functional>
+#include <queue>
 
 #define BEGIN(x) namespace x {
 #define END(x) }
 
 BEGIN(lin)
 
-class thread_pool {
+class Task {
+public:
+    template<typename FUNCTION, typename... ARGS>
+    Task(FUNCTION &&func, ARGS... args) {
+        this->func = std::bind(func, std::forward<ARGS>(args)...);
+    }
+    void run() {
+        std::cout << "Task run : ";
+        func();
+        return ;
+    }
+private:
+    std::function<void()> func;
+};
+
+class ThreadPool {
 public:
 
 private:
     std::vector<std::thread *> threads;
-    std::queue<
+    std::queue<Task *> tasks;
 };
 
 END(lin)
+
+void func(int a, int b, int c) {
+    std::cout << a + b + c << std::endl;
+    return ;
+}
+
+int main() {
+    lin::Task t1(func, 1, 2, 3);
+    std::cout << "hello world" << std::endl;
+    t1.run();
+    return 0;
+}
