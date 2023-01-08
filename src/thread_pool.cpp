960b4ce1a9a824b35a1b6bb18a91a50ed01853e9
luzelin <luzelin1024@163.com>
2023-01-08 14:33:22 +0000

    bind

--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -10,6 +10,8 @@
 #include <thread>
 #include <functional>
 #include <queue>
+#include <unistd.h>
+#include <unordered_map>
 
 #define BEGIN(x) namespace x {
 #define END(x) }
@@ -33,11 +35,44 @@ private:
 
 class ThreadPool {
 public:
-
+    ThreadPool(int n = 5) {
+        for (int i = 0; i < n; ++i) {
+            threads.push_back(new std::thread(&ThreadPool::workerThread, this));
+        }
+    }
+    template<typename FUNCTION, typename... ARGS>
+    void addOneTask(FUNCTION &&, ARGS...);
+    
 private:
     std::vector<std::thread *> threads;
     std::queue<Task *> tasks;
+    std::unordered_map<std::thread::id, bool> running;
+
+    void workerThread() {
+        std::thread::id id = std::this_thread::get_id();
+        // std::cout << id << std::endl;
+        running[id] = true;
+        while (running[id]) {
+            // get Task
+            Task *t = this->getOneTask();
+            // run Task
+            t->run();
+            // del Task
+            delete t;
+        }
+        return ;
+    }
+    Task *getOneTask() {
+        // TODO
+        return nullptr;
+    }
 };
+
+template<typename FUNCTION, typename... ARGS>
+void ThreadPool::addOneTask(FUNCTION &&func, ARGS... args) {
+    // TODO
+    return ;
+}
 
 END(lin)
 
@@ -48,6 +83,8 @@ void func(int a, int b, int c) {
 
 int main() {
     lin::Task t1(func, 1, 2, 3);
+    lin::ThreadPool tp;
+    sleep(1);
     std::cout << "hello world" << std::endl;
     t1.run();
     return 0;
