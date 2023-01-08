78977b3dfdc0b361023b48ec5c067d33da85eb64
luzelin <luzelin1024@163.com>
2023-01-08 10:49:13 +0000

    src

---
+++ src/thread_pool.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: thread_pool.cpp
+	> Author: luzelin
+	> Mail: luzelin1024@163.com
+	> Created Time: Sun 08 Jan 2023 10:44:40 AM UTC
+ ************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <thread>
+
+#define BEGIN(x) namespace x {
+#define END(x) }
+
+BEGIN(lin)
+
+class thread_pool {
+public:
+
+private:
+    std::vector<std::thread *> threads;
+    std::queue<
+};
+
+END(lin)
